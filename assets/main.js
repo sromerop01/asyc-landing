a5183f7c5aed0fe54e4a200c8826fd636ac27699
sromerop01 <sromerop2001@gmail.com>
2023-01-21 14:26:36 -0500

    Updates

--- assets/main.js
+++ assets/main.js
@@ -25,15 +25,15 @@ async function fetchData(urlApi){
             <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-t-lg overflow-hidden group-hover:opacity-75 lg:aspect-none">
                 <img src="${character.image}" alt="${character.name}" class="w-full">
             </div>
-            <div class=" flex flex-col relative justify-between bg-orange-600 p-3">
-                    <div class="section">
+            <div class=" flex flex-col relative justify-between bg-orange-600 p-3 rounded-b-lg">
+                    <div class="mb-4">
                         <h1 class="font-extrabold text-gray-50 sm:text-2xl md:text-2xl">${character.name}</h1>
-                        <span class="status">${character.status} - ${character.species}</span>
+                        <span class=" text-black">${character.status} - ${character.species}</span>
                     </div>
 
-                    <div class="section">
-                        <h3 class="last-location">Last known location:</h3>
-                        <span class="location">${character.location.name}</span>
+                    <div class="mb-2">
+                        <h3 class="font-extrabold text-gray-50">Last known location:</h3>
+                        <span class="text-black">${character.location.name}</span>
                     </div>
             </div>
         </div>
